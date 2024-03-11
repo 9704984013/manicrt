85cebf2d7a4a9c48bc2e69e2b4434f57474ab2b6
9704984013 <162981101+9704984013@users.noreply.github.com>
2024-03-11 15:58:13 +0530

    Add files via upload

---
+++ day1-python.py
@@ -0,0 +1,49 @@
+print("hello world")
+Day-1 Theory Part
+BARGAINED CHARACTERS
+Controls and data structure - ABC language
+strings and its formats - from c
+regular expression --> pearl
+exception handling --> module 3
+magic methods from --> fortron
+
+Flavours of python
+> CPYTHON -Written in c,most common implementation of python
+>jython -java implementation of the python programming language which is tightly integrated with NET.
+>Brython -Browser python,runs in browsers
+>RubyPython -Bridge between python and Ruby interpreters
+>Micropython -Runs in Micro Controllers.
+
+# object -->the value to be printed is object
+# sep--> seperator
+# Eg
+# a = 8
+# b = 9
+# c = 89
+# print(a, b,c,  sep='$')
+
+# end --> which is used to add the string at the end of the value,
+# #It will not keep the command to stay in same line
+# Eg:
+# print(str(a),b, end='%%')
+# print(str(c))
+
+# !----> variables
+# ?variable is a name which used to identify the value of variable
+# ?which is stored in a specific memory location
+
+# Rules to dedfine variables
+# 1.) variable have to start with only alphabets, underscores
+# 2.) The char following the 1st char can be alphabets, numbers, underscores
+# 3.) No space are allowed 
+# 4.) No special characters like !@#$%^()+
+# 5.) case sensitive
+# 6>) keywords are not allowed as variables
+ 
+
+
+
+
+
+
+      
